22cfeb50798937b1fdef25d8520d0ac54160a287
Breno Alves <breno7572@gmail.com>
2024-07-10 23:12:15 -0300

    Organizações: Adicionando Endpoints de Registro e Busca no arquivo de rotas

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/user-register-controller'
+import { create } from './controllers/org-register-controller'
+import { search } from './controllers/org-search-controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/orgs/register', create)
+  app.get('/orgs/:name', search)
 }
